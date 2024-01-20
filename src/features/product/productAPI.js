78b4da2a939dc68569e2c9e8e96753b9863cd191
Sajid Hussain <sajidmcsbwn@gmail.com>
2024-01-20 13:52:19 +0500

    cartAPI implemented

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 // A mock function to mimic making an async request for data
-export function fetchProduct() {
+export function fetchProducts() {
   return axios.get("http://localhost:8080/products");
 }
