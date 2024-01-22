ad5a2d1b824f06fca8ef24ea3c8f33c248247cf5
Sajid Hussain <sajidmcsbwn@gmail.com>
2024-01-22 11:12:12 +0500

    add and delete cart item api's successfully implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { Product } from "./features/product/Product";
 import { Cart } from "./features/cart/Cart";
 import Header from "./layouts/Header";
+import { useDispatch } from "react-redux";
+import { fetchCartAsync } from "./features/cart/cartSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCartAsync());
+  }, []);
+
   return (
     <>
       <Header />
